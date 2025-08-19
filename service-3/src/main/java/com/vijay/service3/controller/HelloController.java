d483702f91e59bdcd58f3be8fee04236628db48c
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-19 19:50:32 +0530

    Update HelloController.java

--- service-3/src/main/java/com/vijay/service3/controller/HelloController.java
+++ service-3/src/main/java/com/vijay/service3/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Second Deployment service 3...";
+        return "Third Time Deployment service 3...";
     }
 }
