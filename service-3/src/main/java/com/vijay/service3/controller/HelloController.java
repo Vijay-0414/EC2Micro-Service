900be7f29fff3bd1dd871dd91f8a3f65f2e48bc3
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-27 13:42:56 +0530

    Update HelloController.java

--- service-3/src/main/java/com/vijay/service3/controller/HelloController.java
+++ service-3/src/main/java/com/vijay/service3/controller/HelloController.java
@@ -2,11 +2,14 @@ package com.vijay.service3.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/service3") 
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Wednesday Fargate Deployment for SERVICE THREE";
     }
 }
+
