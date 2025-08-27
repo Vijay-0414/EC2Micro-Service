ab8db4abe12dcdfdee715ef8dd9cf600e727b880
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-27 15:03:25 +0530

    Update HelloController.java

--- service-2/src/main/java/com/vijay/service2/controller/HelloController.java
+++ service-2/src/main/java/com/vijay/service2/controller/HelloController.java
@@ -7,9 +7,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/service2") 
 public class HelloController {
+
+    @GetMapping
+    public String root() {
+        return "Service2 root endpoint is alive!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+
     @GetMapping("/hello")
     public String hello() {
-        return "Wednesday Fargate Deployment for SERVICE TWO... Updation Checking...";
+        return "Wednesday Fargate Deployment for SERVICE TWO... UPDATED";
+    }
+
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "SERVICE TWO... Second MAPPING...";
     }
 }
-
