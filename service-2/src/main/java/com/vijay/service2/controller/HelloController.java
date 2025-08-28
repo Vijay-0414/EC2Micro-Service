1a6f44eabdb7da23a844b83785ad107b51e9c2db
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-28 16:38:15 +0530

    Update HelloController.java

--- service-2/src/main/java/com/vijay/service2/controller/HelloController.java
+++ service-2/src/main/java/com/vijay/service2/controller/HelloController.java
@@ -10,21 +10,21 @@ public class HelloController {
 
     @GetMapping("/")
     public String root() {
-        return "Service2 root endpoint is alive!.... VIJAYAPERUMAL";
+        return "Service2 root endpoint is alive!.... VIJAYAPERUMAL...Domain name checking...";
     }
 
     @GetMapping("/health")
     public String health() {
-        return "OK";
+        return "OK from s2";
     }
 
     @GetMapping("/hello")
     public String hello() {
-        return "Thursday Fargate Deployment for SERVICE TWO... UPDATED VIJAYAPERUMAL";
+        return "Thursday Fargate Deployment for SERVICE TWO... UPDATED VIJAYAPERUMAL...Domain name checking...";
     }
 
     @GetMapping("/hello2")
     public String hello2() {
-        return "SERVICE TWO... Second MAPPING...VIJAYAPERUMAL";
+        return "SERVICE TWO... Second MAPPING...VIJAYAPERUMAL...Domain name checking...";
     }
 }
